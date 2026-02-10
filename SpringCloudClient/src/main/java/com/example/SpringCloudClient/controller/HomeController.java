767d6a309a71a3f9191ac898577c7679ed308b87
manojyuvanigit <manojyuvanipsg@gmail.com>
2026-02-10 07:55:09 +0530

    spring cloud cient addition

---
+++ SpringCloudClient/src/main/java/com/example/SpringCloudClient/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.SpringCloudClient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cloud")
+public class HomeController {
+
+    @Value("${app.name}")
+    String appName;
+
+    @GetMapping("/home")
+    public String home(){
+        return "Welcome to Spring Cloud Client! " + appName;
+    }
+}
