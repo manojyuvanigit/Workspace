48ae2a7e7bbb21801798003ea10b45ebead12ee2
manojyuvanigit <manojyuvanipsg@gmail.com>
2026-02-10 08:02:12 +0530

    property update for actuator refresh process

--- SpringCloudClient/src/main/java/com/example/SpringCloudClient/controller/HomeController.java
+++ SpringCloudClient/src/main/java/com/example/SpringCloudClient/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.SpringCloudClient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cloud")
+@RefreshScope
 public class HomeController {
 
     @Value("${app.name}")
